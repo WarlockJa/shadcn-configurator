b63b8eef00a1994d3e99879216cc710663d47ab5
warlockja <warlockja@gmail.com>
2024-09-14 20:20:55 +0300

    temporarily disabling reading localStorage for elements' positions

--- src/components/configurator/hooks/useConfigurator.tsx
+++ src/components/configurator/hooks/useConfigurator.tsx
@@ -8,9 +8,8 @@ import {
   paletteColorsAtom,
   sandboxActiveTypeAtom,
   sandboxColorsAtom,
-  staticDraggableElementsDataAtom,
 } from "@/store/jotai";
-import { useAtom, useSetAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { useEffect, useState } from "react";
 import configuratorInit from "../utils/configuratorInit";
 import { cssVars } from "../settings";
@@ -27,12 +26,13 @@ export default function useConfigurator() {
   const [paletteActiveColor, setPaletteActiveColor] = useAtom(
     paletteActiveColorAtom,
   );
-  const [draggableElementsData, setDraggableElementsData] = useAtom(
-    draggableElementsDataAtom,
-  );
-  const setStaticDraggableElementsData = useSetAtom(
-    staticDraggableElementsDataAtom,
-  );
+  const draggableElementsData = useAtomValue(draggableElementsDataAtom);
+  // const [draggableElementsData, setDraggableElementsData] = useAtom(
+  //   draggableElementsDataAtom,
+  // );
+  // const setStaticDraggableElementsData = useSetAtom(
+  //   staticDraggableElementsDataAtom,
+  // );
 
   // initialisation
   useEffect(() => {
@@ -44,8 +44,9 @@ export default function useConfigurator() {
       setSandboxActiveType(data.type);
       setPaletteColors(data.paletteColors);
       setPaletteActiveColor(data.paletteActiveColor);
-      setDraggableElementsData(data.draggableElementsData);
-      setStaticDraggableElementsData(data.draggableElementsData);
+      //
+      // setDraggableElementsData(data.draggableElementsData);
+      // setStaticDraggableElementsData(data.draggableElementsData);
     } else {
       // generating default values
       const { type, colors } = configuratorInit({ cssVars });
