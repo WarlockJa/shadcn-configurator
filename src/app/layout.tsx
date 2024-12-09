74d3416eaac9c4fa9a033c4d6ca2318e9b02f2c6
warlockja <warlockja@gmail.com>
2024-12-09 16:24:09 +0300

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
+export const runtime = "edge";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
