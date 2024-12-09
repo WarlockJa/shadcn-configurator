c96be0ca8136d6f8009f9b0959ecfd74fa2ea463
warlockja <warlockja@gmail.com>
2024-12-09 17:05:48 +0300

    testing disabled fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,40 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Shadcn-Configurator",
   description:
     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+  openGraph: {
+    title: "Shadcn-Configurator",
+    description:
+      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+    url: "https://shadcn-config.com",
+    siteName: "Shadcn Configurator",
+    images: "https://warlockja.com/images/shadcn-config.webp",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shadcn-Configurator",
+    description:
+      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+    images: "https://warlockja.com/images/shadcn-config.webp",
+    site: "@RomanStepa49093",
+    creator: "@RomanStepa49093",
+  },
 };
 
 export default function RootLayout({
@@ -28,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
         <Toaster />
