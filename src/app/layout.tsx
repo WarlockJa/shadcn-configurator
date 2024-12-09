3b63d3c8a664ccaf930087fd54d2b7fd0567d6cb
warlockja <warlockja@gmail.com>
2024-12-09 16:36:38 +0300

    testing no metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,23 +18,6 @@ export const metadata: Metadata = {
   title: "Shadcn-Configurator",
   description:
     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
-  openGraph: {
-    title: "Shadcn-Configurator",
-    description:
-      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
-    url: "https://shadcn-config.com",
-    siteName: "Shadcn Configurator",
-    images: "https://warlockja.com/images/shadcn-config.webp",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Shadcn-Configurator",
-    description:
-      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
-    images: "https://warlockja.com/images/shadcn-config.webp",
-    site: "@RomanStepa49093",
-    creator: "@RomanStepa49093",
-  },
 };
 
 export default function RootLayout({
