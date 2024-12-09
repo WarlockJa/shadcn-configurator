5d4d4ce1ec3b0a0f144e12b824e08248a93400ec
warlockja <warlockja@gmail.com>
2024-12-09 16:04:47 +0300

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,14 +24,14 @@ export const metadata: Metadata = {
       "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
     url: "https://shadcn-config.com",
     siteName: "Shadcn Configurator",
-    images: "https://warlockja.com/images/shadcn-config.webp",
+    // images: "https://warlockja.com/images/shadcn-config.webp",
   },
   twitter: {
     card: "summary_large_image",
     title: "Shadcn-Configurator",
     description:
       "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-    images: "https://warlockja.com/images/shadcn-config.webp",
+    // images: "https://warlockja.com/images/shadcn-config.webp",
     site: "@RomanStepa49093",
     creator: "@RomanStepa49093",
   },
---
+++ src/app/twitter-image.tsx
@@ -0,0 +1,5 @@
+export const alt = "Shadcn Configurator";
+
+export const size = { width: 1465, height: 784 };
+
+export const images = "https://warlockja.com/images/shadcn-config.webp";
