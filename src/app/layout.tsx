d36e58d2267d43c8dc551562a6ae861a2ac2170a
warlockja <warlockja@gmail.com>
2024-12-09 16:26:39 +0300

    testing no metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
-export const runtime = "edge";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,23 +18,23 @@ export const metadata: Metadata = {
   title: "Shadcn-Configurator",
   description:
     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-  openGraph: {
-    title: "Shadcn-Configurator",
-    description:
-      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-    url: "https://shadcn-config.com",
-    siteName: "Shadcn Configurator",
-    images: "https://warlockja.com/images/shadcn-config.webp",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Shadcn-Configurator",
-    description:
-      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-    images: "https://warlockja.com/images/shadcn-config.webp",
-    site: "@RomanStepa49093",
-    creator: "@RomanStepa49093",
-  },
+  // openGraph: {
+  //   title: "Shadcn-Configurator",
+  //   description:
+  //     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
+  //   url: "https://shadcn-config.com",
+  //   siteName: "Shadcn Configurator",
+  //   images: "https://warlockja.com/images/shadcn-config.webp",
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Shadcn-Configurator",
+  //   description:
+  //     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
+  //   images: "https://warlockja.com/images/shadcn-config.webp",
+  //   site: "@RomanStepa49093",
+  //   creator: "@RomanStepa49093",
+  // },
 };
 
 export default function RootLayout({
