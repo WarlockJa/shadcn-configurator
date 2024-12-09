8946c111bc961af370aa5886608412aca852d127
warlockja <warlockja@gmail.com>
2024-12-09 16:30:46 +0300

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,24 +17,24 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Shadcn-Configurator",
   description:
-    "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-  // openGraph: {
-  //   title: "Shadcn-Configurator",
-  //   description:
-  //     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-  //   url: "https://shadcn-config.com",
-  //   siteName: "Shadcn Configurator",
-  //   images: "https://warlockja.com/images/shadcn-config.webp",
-  // },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "Shadcn-Configurator",
-  //   description:
-  //     "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui's tailwind variables.",
-  //   images: "https://warlockja.com/images/shadcn-config.webp",
-  //   site: "@RomanStepa49093",
-  //   creator: "@RomanStepa49093",
-  // },
+    "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+  openGraph: {
+    title: "Shadcn-Configurator",
+    description:
+      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+    url: "https://shadcn-config.com",
+    siteName: "Shadcn Configurator",
+    images: "https://warlockja.com/images/shadcn-config.webp",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shadcn-Configurator",
+    description:
+      "Generate shadcn/ui variables on the fly! A web application for dynamic exploration and visualization of shadcn/ui tailwind variables.",
+    images: "https://warlockja.com/images/shadcn-config.webp",
+    site: "@RomanStepa49093",
+    creator: "@RomanStepa49093",
+  },
 };
 
 export default function RootLayout({
